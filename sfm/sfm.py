51726b1c38cbed08713abca37edeb8cea93a2b40
Antti Yli-Kujala <a.ylikujala@gmail.com>
2022-12-21 19:04:25 +0200

    camera calibration

--- sfm/sfm.py
+++ sfm/sfm.py
@@ -2,19 +2,26 @@
 from utils import readImages, showImage, showFeatures, showMatches
 from feature_detection import orb
 from feature_matching import bfMatch
+from camera_calibration import parameters
 
 def main():  # pragma: no cover
 
+    ## camera calibration
+    dir = "images/calibration_images"
+    calibration_images = readImages(dir)
+    parameters(calibration_images)
+
+
     ## read images
-    dir = "images"
+    dir = "images/boat_images"
     images = readImages(dir)
     #showImage(images[0])
 
     ## feature detection
-    #orb[i] = [kp, des]
+    ## orb[i] = [kp, des]
     orb_f = orb(images)
     #showFeatures(orb_f, images)
 
-    #feature matching
+    ## feature matching
     matches = bfMatch(orb_f[0], orb_f[1])
     showMatches(images[0], images[1], orb_f[0][0], orb_f[1][0], matches)
