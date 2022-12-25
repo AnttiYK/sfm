498ff43bb816fc2346169fb8233357580df6be13
Antti Yli-Kujala <a.ylikujala@gmail.com>
2022-12-25 19:30:50 +0200

    kandi

--- sfm/visualization.py
+++ sfm/visualization.py
@@ -2,10 +2,17 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 def dspace():
-    fig = plt.figure(figsize=(4,4))
-    ax = fig.add_subplot(111, projection='3d')
+    fig = plt.figure()
+    ax = fig.add_subplot(1,2,1, projection='3d')
     ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_zlabel("z")
-    #ax.axis('off')
+    ax.set_ylabel("z")
+    ax.set_zlabel("y")
+    plt.grid()
+    plt.plot(5,5,5,marker = 'o')
+ 
+    bx = fig.add_subplot(1, 2, 2)
+    bx.set_xlabel('x')
+    bx.set_ylabel('y')
+    plt.grid()
+    plt.plot(5,5, marker = 'o')
     plt.show()
