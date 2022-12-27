bdba94080979a5c796ae4f5f9d44099631ec73bc
Antti Yli-Kujala <a.ylikujala@gmail.com>
2022-12-27 18:02:22 +0200

    added visualization of image plane

--- sfm/visualization.py
+++ sfm/visualization.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.art3d as art3d
 import numpy as np
 
 ## shows point in 2d and 3d space 
@@ -7,8 +8,8 @@ def dspace():
     fig = plt.figure()
     ax = fig.add_subplot(1,2,1, projection='3d')
     ax.set_xlabel("x")
-    ax.set_ylabel("z")
-    ax.set_zlabel("y")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
     plt.grid()
     plt.plot(5,5,5,marker = 'o')
  
@@ -24,10 +25,12 @@ def worldC():
     fig = plt.figure()
     ax = fig.add_subplot(1,2,1, projection='3d')
     ax.set_xlabel("x")
-    ax.set_ylabel("z")
-    ax.set_zlabel("y")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
     plt.grid()
+    ## point P
     plt.plot(5,5,5,marker = 'o', color = 'blue')
+    ## world coordinate origin
     plt.plot(0, 0, 0, marker = 'x', color = 'red')
     plt.plot((0,3), (0,0), (0,0), color = 'red')
     plt.plot((0,0),(0,-3), (0,0), color = 'red')
@@ -47,11 +50,14 @@ def camC():
     ax.set_ylabel("z")
     ax.set_zlabel("y")
     plt.grid()
+    ## point P
     plt.plot(5,5,5,marker = 'o', color = 'blue')
+    ## world coordinate origin
     plt.plot(0, 0, 0, marker = 'x', color = 'red')
     plt.plot((0,3), (0,0), (0,0), color = 'red')
     plt.plot((0,0),(0,-3), (0,0), color = 'red')
     plt.plot((0,0), (0,0), (0,3), color = 'red')
+    ## camera coordinate origin
     plt.plot(15, 6, 3, marker = 'x', color = 'green')
     plt.plot((15,18), (6,6), (3,3), color = 'green')
     plt.plot((15,15),(6,3), (3,3), color = 'green')
@@ -68,23 +74,39 @@ def imgC():
     fig = plt.figure()
     ax = fig.add_subplot(1,2,1, projection='3d')
     ax.set_xlabel("x")
-    ax.set_ylabel("z")
-    ax.set_zlabel("y")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
     plt.grid()
+    ## point P
     plt.plot(5,5,5,marker = 'o', color = 'blue')
+    ## world coordinate origin
     plt.plot(0, 0, 0, marker = 'x', color = 'red')
     plt.plot((0,3), (0,0), (0,0), color = 'red')
     plt.plot((0,0),(0,-3), (0,0), color = 'red')
     plt.plot((0,0), (0,0), (0,3), color = 'red')
+    ## line from world origin to P
+    plt.plot((0,5), (0,5), (0,5), color = 'red', linestyle = 'dashed')
+    ## camera coordinate origin
     plt.plot(15, 6, 3, marker = 'x', color = 'green')
     plt.plot((15,18), (6,6), (3,3), color = 'green')
     plt.plot((15,15),(6,3), (3,3), color = 'green')
     plt.plot((15,15), (6,6), (3,6), color = 'green')
+    ## line from camera coordinate origin to P
+    plt.plot((15,5), (6,5), (3,5), color = 'green', linestyle = 'dashed')
+    ## optical axis
     plt.plot((15,-10), (6,6), (3,3), color = 'black')
-    points = np.array([[10, 1, -2], [10, 11, 8], [10, 11, -2], [10, 1, 8]])
-    ax.scatter3D(points[0,:], points[1,:], points[2,:], points[3,:])
+    plt.plot((5,5), (6,5), (3,3), color = 'black')
+    plt.plot((5,5), (5,5), (3,5), color = 'black')
+    plt.plot((10,10), (6,5.5), (3,3), color = 'black')
+    plt.plot((10,10), (5.5, 5.5), (3, 4), color = 'black')
+    plt.plot(10, 5.5, 4, marker = 'o', color = 'black')
+    ## image plane
+    rec = plt.Rectangle((3,2), 5, 5, fc=(0,0,1,0.2))
+    ax.add_patch(rec)
+    art3d.patch_2d_to_3d(rec, z=10, zdir='x')
     ax.text(1,0,0, 'origin')
-    ax.text(5,5,6, '(Xc, Yc, Zc) P (Xw, Yw, Zw)')
+    ax.text(5,5,6, 'P')
+    ax.text(10, 5.5, 5, 'P\'')
     ax.text(16,6,3, 'camera')
     ax.text(-10, 6, 3, 'optical axis')
     ax.set_xlim(-10,20)
