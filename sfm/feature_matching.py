c2808f126a71ef88a71ea6ca33f3ce2365bbf341
Antti Yli-Kujala <a.ylikujala@gmail.com>
2023-02-13 09:52:03 +0200

    fixed mask

--- sfm/feature_matching.py
+++ sfm/feature_matching.py
@@ -1,7 +1,9 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
-
+'''
+Brute force matching for 
+'''
 def bfMatch(features):
     matcher = cv.BFMatcher(cv.NORM_HAMMING, crossCheck = True)
     matches = []
@@ -24,16 +26,25 @@ def perspective(images, features, matches):
             src = np.float32([features[i][0][m.queryIdx].pt for m in matches[i][j]]).reshape(-1, 1, 2)
             dst = np.float32([features[j][0][m.trainIdx].pt for m in matches[i][j]]).reshape(-1, 1, 2)
             M, mask = cv.findHomography(src, dst, cv.RANSAC, 5.0)
-            matchesMask = mask.ravel().tolist()
+            ## sets mask sum to 0 when image is compared to itself
+            if (i == j):
+                matchesMask = [0]
+            else:
+                matchesMask = mask.ravel().tolist()
             tm = cv.perspectiveTransform(pts, M)
             transformation[i].append([tm, matchesMask])
     return transformation
 
 def showMatches(images, transformations, features, matches):
-    draw_params2 = dict(matchColor = (0, 255, 0), singlePointColor = None, matchesMask = transformations[3][4][1], flags = 2)
+    firstImageIndex = 3
+    secondImageIndex = 2
+    mask = transformations[firstImageIndex][secondImageIndex][1]
+    count = sum(mask)
+    print(count)
+    draw_params2 = dict(matchColor = (0, 255, 0), singlePointColor = None, matchesMask = mask, flags = 2)
     draw_params1 = dict(matchColor = (255, 0, 0), singlePointColor = None, flags = 2)
-    img1 = cv.drawMatches(images[3], features[3][0], images[4], features[4][0], matches[3][4], None, **draw_params1)
-    img2 = cv.drawMatches(images[3], features[3][0], images[4], features[4][0], matches[3][4], None, **draw_params2)
+    img1 = cv.drawMatches(images[firstImageIndex], features[firstImageIndex][0], images[secondImageIndex], features[secondImageIndex][0], matches[firstImageIndex][secondImageIndex], None, **draw_params1)
+    img2 = cv.drawMatches(images[firstImageIndex], features[firstImageIndex][0], images[secondImageIndex], features[secondImageIndex][0], matches[firstImageIndex][secondImageIndex], None, **draw_params2)
     fig = plt.figure()
     ax=fig.add_subplot(1, 2, 1)
     plt.imshow(img1, 'gray')
